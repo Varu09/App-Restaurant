92e1b26146515386720e9341119de8b63052b175
Varu09 <alexandru09grajdan@gmail.com>
2016-12-17 15:56:13 +0200

    Script tabele MySQL

---
+++ tabele.sql
@@ -0,0 +1,69 @@
+drop database if exists `restaurant`;
+create database restaurant;
+use restaurant;
+
+drop table if exists `masa`;
+create table masa (
+	id int not null auto_increment,
+    nr_locuri int not null,
+    primary key(id)   
+);
+
+drop table  if exists `customer`;
+create table customer (
+	id int not null auto_increment,
+    nume varchar(20) not null,
+    prenume varchar(20) not null,
+    nr_masa int not null,
+    primary key(id),
+	foreign key(nr_masa) references masa(id)    
+   );
+   
+drop table  if exists `comanda`;
+create table comanda (
+	id int not null auto_increment,
+    customer_id int not null,
+    primary key(id),
+    foreign key(customer_id) references customer(id)
+); 
+  
+drop table  if exists `nota_de_plata`;
+create table nota_de_plata (
+	id int not null auto_increment,
+	nr_masa int not null,
+    gramaj_total int not null,
+    pret_total numeric not null,
+    primary key(id),
+    foreign key(nr_masa) references masa(id)
+);
+
+drop table if exists `categorie`;
+create table categorie (
+	id int not null auto_increment,
+    denumire varchar(12),
+    primary key(id)
+);
+
+drop table if exists `produs`;
+create table produs (
+	id int not null auto_increment,
+    categorie_id int not null,
+    nume varchar(40) not null,
+    pret numeric not null,
+    gramaj int not null,
+    primary key(id),
+    foreign key(categorie_id) references categorie(id)
+);
+
+drop table if exists `istoric_comenzi`;
+create table istoric_comenzi (
+	id int not null auto_increment,
+    comanda_id int not null,
+    produs_id int not null,
+    primary key(id),
+    foreign key(comanda_id) references comanda(id),
+    foreign key(produs_id) references produs(id)
+)
+
+
+
